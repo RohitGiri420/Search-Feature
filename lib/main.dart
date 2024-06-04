0ab361b0a471b4600c85407660a75a2760022080
Rohit Giri <giri420rohit@gmail.com>
2024-06-04 17:53:02 +0530

    Search on list is implimented

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,105 @@
-import 'package:best_property/second_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
-  runApp(SecondPage());
+void main(){
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    // TODO: implement build
+    return MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  var _counter = 0.obs;
+class _HomeScreenState extends State<HomeScreen> {
 
-  void _incrementCounter() {
-    _counter++;
+  List<Map<String, dynamic>> allNote =[
+    {"title":"rohit","description": "descripton1"},
+    {"title":"ajay","description": "descripton2"},
+    {"title":"asif","description": "descripton3"},
+    {"title":"inam","description": "descripton4"},
+    {"title":"bansal","description": "descripton5"},
+    {"title":"divanshu","description": "descripton6"},
+    {"title":"hanan","description": "descripton7"},
+    {"title":"harshit","description": "descripton8"},
+  ];
+
+  List<Map<String,dynamic>> foundNotes=[];
+
+  _searchNote(String ChangedValue){
+    List<Map<String,dynamic>>result=[];
+
+    if(ChangedValue.isEmpty){
+      result = allNote;
+    }
+    else{
+     result = allNote.where((element) => element["title"].toLowerCase().contains(ChangedValue.toLowerCase()),).toList();
+    }
+
+    setState(() {
+      foundNotes = result;
+    });
+
   }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    foundNotes=allNote;
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return
+      Scaffold(
+        appBar: AppBar(),
+        body: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(height: 10,),
+                TextField(
+                  onChanged: (value) {
+                    _searchNote(value);
+                  },
+                    decoration: InputDecoration(
+                    border: OutlineInputBorder()
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Expanded(
+                  child: ListView.builder(itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        title: Text("${foundNotes[index]["title"]}"),
+                        subtitle: Text("${foundNotes[index]["description"]}"),
+                      ),
+                    );
+                  },
+                  itemCount: foundNotes.length,),
+                ),
+              ],
             ),
-            Obx(()=>Text(_counter.string)),
+          ),
 
-            ElevatedButton(onPressed: () {
-              Get.snackbar("title", "message");
-            }, child: Text('Snacbar')),
-            
-            ElevatedButton(onPressed: () {
-              Get.defaultDialog(title: "dialog box");
-            }, child: Text("alert box")),
-
-            ElevatedButton(onPressed: () {
-              Get.bottomSheet(Container(width: 400,height: 400,));
-            }, child: Text("Bottom sheet")),
-
-            ElevatedButton(onPressed: () {
-              Get.to(SecondPage());
-            }, child: Text("Second page")),
-            ElevatedButton(onPressed: () {
-              Get.changeTheme(ThemeData.dark());
-            }, child: Text("Dark Mode")),
-
-          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+      );
   }
 }
